474439426a8bae1371ea4578ea8a56d70dd17fb0
Igor Kushnir <igor.kushnir@perfsol.tech>
2025-11-26 16:04:57 +0200

    Update next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['zcejeej0ras6rj8h.public.blob.vercel-storage.com'],
+    domains: [
+      'zcejeej0ras6rj8h.public.blob.vercel-storage.com',
+      'd0jo4e8kojckav6k.public.blob.vercel-storage.com',
+    ],
   },
 }
 
